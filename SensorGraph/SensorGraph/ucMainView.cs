dcf108a35b198cbc902b182d27fbd2e81109133e
李元龍 <wonyong.lee@techwing.co.kr>
2021-04-22 21:13:35 +0900

    parsing

--- SensorGraph/SensorGraph/ucMainView.cs
+++ SensorGraph/SensorGraph/ucMainView.cs
@@ -161,18 +161,26 @@ namespace SensorGraph
 
 		private void BwGraphRefresh1_ProgressChanged( object sender, ProgressChangedEventArgs e )
 		{
-			var v = ( List<double> )e.UserState;
-			ucGraph1_1.add_value( v[ 0 ] ); g1_1.Add( v[ 0 ] );
-			ucGraph1_2.add_value( v[ 1 ] ); g1_2.Add( v[ 1 ] );
-			ucGraph1_3.add_value( v[ 2 ] ); g1_3.Add( v[ 2 ] );
-			ucGraph1_4.add_value( v[ 3 ] ); g1_4.Add( v[ 3 ] );
+			try
+			{
+				var v = ( List<double> )e.UserState;
+				ucGraph1_1.add_value( v[ 0 ] ); g1_1.Add( v[ 0 ] );
+				ucGraph1_2.add_value( v[ 1 ] ); g1_2.Add( v[ 1 ] );
+				ucGraph1_3.add_value( v[ 2 ] ); g1_3.Add( v[ 2 ] );
+				ucGraph1_4.add_value( v[ 3 ] ); g1_4.Add( v[ 3 ] );
 
-			ucGraph1_1.set_min( g1_1.Min() ); ucGraph1_1.set_max( g1_1.Max() ); ucGraph1_1.set_ave( g1_1.Average() );
-			ucGraph1_2.set_min( g1_2.Min() ); ucGraph1_2.set_max( g1_2.Max() ); ucGraph1_2.set_ave( g1_2.Average() );
-			ucGraph1_3.set_min( g1_3.Min() ); ucGraph1_3.set_max( g1_3.Max() ); ucGraph1_3.set_ave( g1_3.Average() );
-			ucGraph1_4.set_min( g1_4.Min() ); ucGraph1_4.set_max( g1_4.Max() ); ucGraph1_4.set_ave( g1_4.Average() );
+				ucGraph1_1.set_min( g1_1.Min() ); ucGraph1_1.set_max( g1_1.Max() ); ucGraph1_1.set_ave( g1_1.Average() );
+				ucGraph1_2.set_min( g1_2.Min() ); ucGraph1_2.set_max( g1_2.Max() ); ucGraph1_2.set_ave( g1_2.Average() );
+				ucGraph1_3.set_min( g1_3.Min() ); ucGraph1_3.set_max( g1_3.Max() ); ucGraph1_3.set_ave( g1_3.Average() );
+				ucGraph1_4.set_min( g1_4.Min() ); ucGraph1_4.set_max( g1_4.Max() ); ucGraph1_4.set_ave( g1_4.Average() );
 
-			times1.Add( DateTime.Now );
+				times1.Add( DateTime.Now );
+			}
+			catch( Exception )
+			{
+
+			}
+			
 		}
 
 		private void BwGraphRefresh1_DoWork( object sender, DoWorkEventArgs e )
@@ -185,33 +193,97 @@ namespace SensorGraph
 				if( bwGraphRefresh1.CancellationPending )
 					break;
 
-				List<double> d = new List<double>() { 0, 0, 0, 0 };
-				System.Threading.Thread.Sleep( 1 );
-
-				if( simul )
+				try
 				{
-					System.Threading.Thread.Sleep( 100 );
+					List<double> d = new List<double>() { 0, 0, 0, 0 };
+					System.Threading.Thread.Sleep( 1 );
 
-					Random rand = new Random();
-					d[0] = ( rand.Next( 5000 ) / 100.0 );
-					d[1] = ( rand.Next( 5000 ) / 100.0 );
-					d[2] = ( rand.Next( 5000 ) / 100.0 );
-					d[3] = ( rand.Next( 5000 ) / 100.0 );
+					if( simul )
+					{
+						System.Threading.Thread.Sleep( 100 );
+
+						Random rand = new Random();
+						d[ 0 ] = ( rand.Next( 5000 ) / 100.0 );
+						d[ 1 ] = ( rand.Next( 5000 ) / 100.0 );
+						d[ 2 ] = ( rand.Next( 5000 ) / 100.0 );
+						d[ 3 ] = ( rand.Next( 5000 ) / 100.0 );
+					}
+					else
+					{
+						//byte[] bytes = udp1.Receive( ref epRemote );
+
+						//byte[] bytes = { 0x02, 0x14, 0x81, 0x02, 0x14, 0x02, 0x13, 0x02, 0x11, 0x02, 0x12, 0x00, 0xDF, 0x02, 0xE2, 0x01, 0x00, 0x00, 0xAD, 0x03 };
+						byte[] bytes = { 0x02, 0x14, 0x81, 0x80, 0x64, 0x02, 0x13, 0x02, 0x11, 0x02, 0x12, 0x00, 0xDF, 0x02, 0xE2, 0x01, 0x00, 0x00, 0xAD, 0x03 };
+
+						string str = Encoding.Default.GetString( bytes );
+						log( str );
+
+						// 0x02    0x14    0x81    0x02    0x14    0x02    0x13    0x02    0x11    0x02    0x12    0x00    0xDF    0x02    0xE2    0x01    0x00    0x00    0xAD    0x03
+
+						if( bytes.Count() != 20 )
+							continue;
+
+						if( bytes[ 0 ] != 0x02 )
+							continue;
+
+						if( bytes[ 19 ] != 0x03 )
+							continue;
+
+						short ch1 = 0x0000;
+						ch1 |= ( short )( bytes[ 3 ] << 8 );
+						ch1 |= ( short )bytes[ 4 ];
+
+						if( (ch1 & 0x8000) == 0x8000 )
+						{
+							ch1 = ( short )(ch1 ^ 0x8000);
+							ch1 *= -1;
+						}
+
+						int ch2 = 0x0000;
+						ch2 |= bytes[ 5 ] << 8;
+						ch2 |= bytes[ 6 ];
+
+						int ch3 = 0x0000;
+						ch3 |= bytes[ 7 ] << 8;
+						ch3 |= bytes[ 8 ];
+
+						int ch4 = 0x0000;
+						ch4 |= bytes[ 9 ] << 8;
+						ch4 |= bytes[ 10 ];
+
+						//short s = 0x8064;
+						//s = Convert.ToInt16( "0x8064", 16 );
+						//s = (short)( ch1 << 16 );
+						//s = (short)(s << 16);
+						//s = Convert.ToInt16( (short)(ch1<<16) );
+						//s &= 0x00ff;
+						d[ 0 ] = ch1;
+						d[ 1 ] = ch2;
+						d[ 2 ] = ch3;
+						d[ 3 ] = ch4;
+					}
+
+					bwGraphRefresh1.ReportProgress( 0, d );
 				}
-				else
-				{					
-					byte[] bytes = udp1.Receive( ref epRemote );
-					string str = Encoding.Default.GetString( bytes );
-					log( str );
+				catch( Exception ex )
+				{
+					Console.WriteLine( ex.Message );
 				}
 
-				bwGraphRefresh1.ReportProgress( 0, d );
+				
 			}
 		}
 
 		private void BwGraphRefresh1_RunWorkerCompleted( object sender, RunWorkerCompletedEventArgs e )
 		{
-			save_data( eAxis.Axis1, ref times1, ref g1_1, ref g1_2, ref g1_3, ref g1_4 );
+			try
+			{
+				save_data( eAxis.Axis1, ref times1, ref g1_1, ref g1_2, ref g1_3, ref g1_4 );
+			}
+			catch( Exception )
+			{
+
+			}
 		}
 
 
@@ -222,19 +294,27 @@ namespace SensorGraph
 
 		private void BwGraphRefresh2_ProgressChanged( object sender, ProgressChangedEventArgs e )
 		{
-			var v = ( List<double> )e.UserState;
+			try
+			{
+				var v = ( List<double> )e.UserState;
 
-			ucGraph2_1.add_value( v[ 0 ] ); g2_1.Add( v[ 0 ] );
-			ucGraph2_2.add_value( v[ 1 ] ); g2_2.Add( v[ 1 ] );
-			ucGraph2_3.add_value( v[ 2 ] ); g2_3.Add( v[ 2 ] );
-			ucGraph2_4.add_value( v[ 3 ] ); g2_4.Add( v[ 3 ] );
+				ucGraph2_1.add_value( v[ 0 ] ); g2_1.Add( v[ 0 ] );
+				ucGraph2_2.add_value( v[ 1 ] ); g2_2.Add( v[ 1 ] );
+				ucGraph2_3.add_value( v[ 2 ] ); g2_3.Add( v[ 2 ] );
+				ucGraph2_4.add_value( v[ 3 ] ); g2_4.Add( v[ 3 ] );
 
-			ucGraph2_1.set_min( g2_1.Min() ); ucGraph2_1.set_max( g2_1.Max() ); ucGraph2_1.set_ave( g2_1.Average() );
-			ucGraph2_2.set_min( g2_2.Min() ); ucGraph2_2.set_max( g2_2.Max() ); ucGraph2_2.set_ave( g2_2.Average() );
-			ucGraph2_3.set_min( g2_3.Min() ); ucGraph2_3.set_max( g2_3.Max() ); ucGraph2_3.set_ave( g2_3.Average() );
-			ucGraph2_4.set_min( g2_4.Min() ); ucGraph2_4.set_max( g2_4.Max() ); ucGraph2_4.set_ave( g2_4.Average() );
+				ucGraph2_1.set_min( g2_1.Min() ); ucGraph2_1.set_max( g2_1.Max() ); ucGraph2_1.set_ave( g2_1.Average() );
+				ucGraph2_2.set_min( g2_2.Min() ); ucGraph2_2.set_max( g2_2.Max() ); ucGraph2_2.set_ave( g2_2.Average() );
+				ucGraph2_3.set_min( g2_3.Min() ); ucGraph2_3.set_max( g2_3.Max() ); ucGraph2_3.set_ave( g2_3.Average() );
+				ucGraph2_4.set_min( g2_4.Min() ); ucGraph2_4.set_max( g2_4.Max() ); ucGraph2_4.set_ave( g2_4.Average() );
 
-			times2.Add( DateTime.Now );
+				times2.Add( DateTime.Now );
+			}
+			catch( Exception )
+			{
+
+			}
+			
 		}
 
 		private void BwGraphRefresh2_DoWork( object sender, DoWorkEventArgs e )
@@ -273,7 +353,14 @@ namespace SensorGraph
 
 		private void BwGraphRefresh2_RunWorkerCompleted( object sender, RunWorkerCompletedEventArgs e )
 		{
-			save_data( eAxis.Axis2, ref times2, ref g2_1, ref g2_2, ref g2_3, ref g2_4 );
+			try
+			{
+				save_data( eAxis.Axis2, ref times2, ref g2_1, ref g2_2, ref g2_3, ref g2_4 );
+			}
+			catch( Exception )
+			{
+
+			}
 		}
 
 
